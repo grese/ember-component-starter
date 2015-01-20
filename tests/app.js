68e502a4050b46c85e5ef9f15129eafe42a550e6
grese <grese@yahoo-inc.com>
2015-01-20 12:26:25 -0800

    added a couple of divs to the dom for qunit

--- tests/app.js
+++ tests/app.js
@@ -5,6 +5,12 @@ App.rootElement = '#ember-testing';
 App.setupForTesting();
 App.injectTestHelpers();
 App.reset();
+if (Ember.$('#qunit').length === 0) {
+	Ember.$('<div id="qunit"/>').appendTo(document.body);
+}
+if (Ember.$('#qunit-fixture').length === 0) {
+	Ember.$('<div id="qunit-fixture"/>').appendTo(document.body);
+}
 if (Ember.$('#ember-testing').length === 0) {
 	Ember.$('<div id="ember-testing"/>').appendTo(document.body);
 }
